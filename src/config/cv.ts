0fb8faa811f165112c629342bc1b9fbcdb184199
Hendryawan <hendryawan.cipta@gmail.com>
2026-05-13 07:18:05 +0700

    Changing cv link

--- src/config/cv.ts
+++ src/config/cv.ts
@@ -1,2 +1,2 @@
 export const cv_link =
-  "https://drive.google.com/file/d/1KXlMVxsiehDpw405t0-iO8Z57bpVEqQP/view?usp=sharing";
+  "https://drive.google.com/file/d/1WROKzOCJeorCTpBWoIr03joUKkuw6G6l/view?usp=sharing";
